4391127d0de06a8e54a2c31f1175b403174bdd29
Siddhant Kevit <siddhant_prajapati@kevit.io>
2023-09-11 12:46:24 +0530

    Ignore package-lock.json using .gitignore

---
+++ src/db/mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex : true
+})
+
+const User = mongoose.model('User', {
+    name : {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'Siddhant',
+    age: 20
+})
+
+me.save().then(()=>{
+    console.log(me)
+}).catch((error)=> console.log(error))
