416d2a9721b3a87b43f291bbc0f57668e03b4cb8
Siddhant Kevit <siddhant_prajapati@kevit.io>
2023-09-13 13:37:53 +0530

    generate new when user signup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./src/db/mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,14 +16,7 @@ app.listen(port, ()=>{
     console.log('server is running on port '+ port)
 })
 
-// myPassword = async () => {
-//     const mypass = 'riya12785'
-//     const hashPass = '$2b$08$f7sd.8stZNrGTv1yo4ob.O4JHFL6JDUCH.0CFFLc5Px1kjUe3BB1m'
-//     const hashPass1 = await bcrypt.hash(mypass, 8)
-//     console.log(mypass)
-//     console.log(hashPass)
-//     console.log(hashPass1)
-//     const isMatch = await bcrypt.compare(mypass, hashPass1)
-//     console.log(isMatch)
-// }
-// myPassword()
+myFunction = async () => {
+    
+}
+myFunction()
